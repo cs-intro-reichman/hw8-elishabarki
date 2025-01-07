cde254c5108cfc8a61732978d573ed0b093a85b7
elishabarki <elisha.barki@gmail.com>
2025-01-07 10:36:48 +0200

    yallaaa

--- User.java
+++ User.java
@@ -43,24 +43,26 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        for (int i = 0; i < fCount; i++) { 
+        for (int i = 0; i < follows.length; i++) {
             if (follows[i] != null && follows[i].equals(name)) {
                 return true;
             }
         }
         return false; 
-    }    
+    }
+    
     
     
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         if (fCount >= maxfCount || follows(name)) {
-            return false; 
+            return false;
         }
         follows[fCount++] = name; 
         return true;
     }
+    
     
     
     
